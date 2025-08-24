96ee98407ebb8d4d643b73a0368499aeb1113100
mrtechnic <osita.ezikeugo22@gmail.com>
2025-08-24 20:43:36 +0100

    updated httpOnly and samesite

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -32,20 +32,8 @@ const user = new User({
 
     await user.save();
 
-    // Automatically log in the user after registration
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1d' });
-
-    res.cookie(authCookieName, token, {
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
-      httpOnly: true, // prevents JavaScript access (more secure)
-      secure: process.env.NODE_ENV === "production", // cookie only sent over HTTPS in production
-      sameSite: "none", // prevents CSRF attacks
-      path: "/", // cookie is valid for the whole site
-    });
-
     res.status(201).json({ 
       message: 'User registered successfully',
-      user: { id: user._id, name: user.name, email: user.email }
     });
   } catch (error) {
     console.error('Registration error:', error);
@@ -78,9 +66,9 @@ export const login = async (req: Request, res: Response): Promise<void> => {
 
 res.cookie(authCookieName, token, {
   maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
-  httpOnly: true, // prevents JavaScript access (more secure)
+  httpOnly: false, // prevents JavaScript access (more secure)
   secure: process.env.NODE_ENV === "production", // cookie only sent over HTTPS in production
-  sameSite: "none", // prevents CSRF attacks
+  sameSite: "lax", // prevents CSRF attacks
   path: "/", // cookie is valid for the whole site
 });
 
@@ -112,10 +100,7 @@ export const fetchLoggedInUser = async (req: Request, res: Response): Promise<vo
 export const logOutUser = async (req: Request, res: Response): Promise<void> => {
   try {
     res.clearCookie(authCookieName, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
-      path: "/",
+
     });
     res.status(200).json({ message: 'Logout successful' });
   } catch (error) {
